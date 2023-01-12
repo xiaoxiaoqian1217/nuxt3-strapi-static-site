ac0d92b0d4bfa5f80eb296e5ce5c86c904811f42
xiaoxiaoqian1217 <xiaoxiaoqian581217>
2023-01-12 15:29:05 +0800

    feat: 添加seo内容

---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+ <div
+              v-for="navigation in siteHeader.navigations.data"
+              :key="navigation.href"
+            >
+              <NuxtLink :to="navigation.href" :target="navigation.target"
+                >{{ navigation.label }}
+              </NuxtLink>
+            </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { twGlobalQurey } from "../graphql/twGlobal";
+import { getSlugSeoQuery } from "../graphql/slugSeo";
+const {
+  public: { strapiURL },
+  
+} = useRuntimeConfig();
+const route = useRoute();
+const slug = route.name;
+
+const { data :globalData } = await useAsyncQuery(twGlobalQurey);
+// 获取SEO数据
+const { data: SeoData } = await useAsyncQuery(getSlugSeoQuery, {
+  slug: slug || "index",
+});
+const data = removeAttrsAndId(removeTime(unref(globalData)));
+const {
+  twGlobal: {
+    data: { siteHeader, footer },
+  },
+} = data;
+const { Seo } = removeAttrsAndId(removeTime(SeoData.value))?.pages?.data[0];
+console.log('%c [ Seo ]-36', 'font-size:13px; background:pink; color:#bf2c9f;', Seo)
+useHead({
+  title: Seo?.metaTitle,
+
+  meta: [
+    {
+      name: "description",
+      content: Seo?.metaDescription || "",
+    },
+     {
+      name: "keywords",
+      content: Seo?.keywords || "",
+    },
+  ],
+  bodyAttrs: {
+    class: "test",
+  },
+});
+
+</script>
+
+<style scoped>
+
+</style>
